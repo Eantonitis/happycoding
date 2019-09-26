dae96d481a8492eb4a8bf2e759b7f1be535d5240
Eantonitis <51980622+Eantonitis@users.noreply.github.com>
2019-09-26 12:13:33 -0500

    Delete AddingTwoNumbers.py

--- AddingTwoNumbers.py
+++
@@ -1,6 +0,0 @@
-num1 = (float(input("What is your first number?: ")))
-num2 = (float(input("What is your second number?: ")))
-
-total = num1 + num2
-
-print(f"{total} is the total of your two numbers.")
